6a606548a3f079e61a3addb173db0dc9d86bcf0f
giahuy102 <ghuy102@gmail.com>
2023-04-30 21:22:33 +0700

    [ADD] Add docker

--- ai_server/__init__.py
+++ ai_server/__init__.py
@@ -1,21 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
 
-from .internal.delivery.rabbitmq.consumers import Consumers
-from .internal.delivery.stream_detector.stream_detector import StreamDetector
-from .internal.delivery.grpc.server import GrpcServer
+# from .internal.delivery.rabbitmq.consumers import Consumers
+# from .internal.delivery.stream_detector.stream_detector import StreamDetector
+# from .internal.delivery.grpc.server import GrpcServer
 
-def main():
-    consumers = Consumers()
-    consumers.start()
+# def main():
+#     consumers = Consumers()
+#     consumers.start()
 
-    grpc_server = GrpcServer()
-    grpc_server.start()
+#     grpc_server = GrpcServer()
+#     grpc_server.start()
 
-    stream_detector = StreamDetector()
-    stream_detector.start()
+#     stream_detector = StreamDetector()
+#     stream_detector.start()
 
 
 
-main()
+# main()
---
+++ ai_server/cmd/main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+
+from ai_server.internal.delivery.rabbitmq.consumers import Consumers
+from ai_server.internal.delivery.stream_detector.stream_detector import StreamDetector
+from ai_server.internal.delivery.grpc.server import GrpcServer
+
+def main():
+    consumers = Consumers()
+    consumers.start()
+
+    grpc_server = GrpcServer()
+    grpc_server.start()
+
+    stream_detector = StreamDetector()
+    stream_detector.start()
+    app = Flask(__name__)
+    app.run(host='0.0.0.0', port=5000)
+
+
+
+main()
