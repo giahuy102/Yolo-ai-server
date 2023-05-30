3d604ebe188fa746c13e57d4b1fbd447371b0490
Huynh Thanh Dat <huynhthanhdat1108@gmail.com>
2023-05-30 22:18:59 +0700

    update dockerfile

--- ai_server/pkg/config/config.py
+++ ai_server/pkg/config/config.py
@@ -2,7 +2,7 @@ import tomli
 import os
 from pathlib import Path
 
-toml_path = os.environ["CONFIG_PATH"]
+toml_path = os.getenv("CONFIG_PATH")
 print("Config path: ", toml_path)
 
 with Path(toml_path).open(mode='rb') as fp:
